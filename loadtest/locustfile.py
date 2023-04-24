c539b9e3b2b2a5482548d09a599d0bf92a5d16ca
Antoni Baum <antoni.baum@protonmail.com>
2023-04-24 14:13:14 -0700

    Fix duration
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>

--- loadtest/locustfile.py
+++ loadtest/locustfile.py
@@ -58,8 +58,8 @@ class StagesShape(LoadTestShape):
         {"duration": 100, "users": 10, "spawn_rate": 10},
         {"duration": 800, "users": 100, "spawn_rate": 0.125},
         {"duration": 1800, "users": 100, "spawn_rate": 100},
-        {"duration": 800, "users": 100, "spawn_rate": 0.125},
-        {"duration": 100, "users": 10, "spawn_rate": 10},
+        {"duration": 3500, "users": 100, "spawn_rate": 0.125},
+        {"duration": 3600, "users": 10, "spawn_rate": 10},
     ]
 
     def tick(self):
